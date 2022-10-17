20a42f697a05c0c63db6e3ed4095541a5392fbe8
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-10-17 13:31:27 +0200

    nowa metoda

--- tests/home/login_test.py
+++ tests/home/login_test.py
@@ -14,14 +14,18 @@ class LoginTest(unittest.TestCase):
         driver.get(baseUrl)
         lp = LoginPage(driver)
         lp.loginValid('test@email.com', 'abcabcABC')
-
+        
+        message_valid = lp.loginNotOK()
+        assert message_valid == 'Your username or password is invalid. Please try again.'       
+        
+        '''
         verifyLogin = driver.find_element(By.XPATH, '//span[@class="dynamic-text help-block"]').text
         
         if verifyLogin is not None:
             print(f'Element znaleziony: {verifyLogin}')
             assert verifyLogin == 'Your username or password is invalid. Please try again.'
         else:
-            print('Nie ma elementu')            
+            print('Nie ma elementu')'''            
 
     def test_loginSucces(self):
         baseUrl = 'https://letskodeit.com/'
@@ -31,14 +35,17 @@ class LoginTest(unittest.TestCase):
         driver.get(baseUrl)
         lp = LoginPage(driver)
         lp.loginSuccesfull('test@email.com', 'abcabc')
-                
+        
+        message_succes = lp.loginOK()
+        assert message_succes == 'My Account'
+        '''        
         loginSuccesfull = driver.find_element(By.XPATH, '//a[contains(text(),"My Account")]').text
                 
         if loginSuccesfull is not None:
             print('Login succesfull')
             assert loginSuccesfull == 'My Account'
         else:
-            print('Nie udało się zalogować')            
+            print('Nie udało się zalogować')'''            
         
         driver.quit()
 
