0cb5eb69e198e3a4146723afff871c9ba96b7435
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-10-18 13:45:00 +0200

    loggi

--- tests/home/register_multi_test.py
+++ tests/home/register_multi_test.py
@@ -12,19 +12,19 @@ class RegisterTest(unittest.TestCase):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window()
     driver.implicitly_wait(3)
+    driver.get(baseUrl)
     rp = RegisterPage(driver)
     
     @data(('JavaScript for beginners', '1234 2345 3456 4567', '10/24', '345', 'Poland'), ('Learn Python 3 from scratch', '1234 2345 3456 4567', '10/24', '345', 'Poland'))
     @unpack
     def test_registerPage(self, namecourse, cardnumber, carddata, cardcode, countryname):
-        self.driver.get(self.baseUrl)
+        
         self.rp.registerCourse(namecourse)
         self.rp.card(cardnumber, carddata, cardcode) 
         self.rp.country(countryname)
                 
         message = self.rp.errorCardNumber()
         assert message == 'Numer karty jest nieprawidłowy.' 
-        
-        # self.rp.clickAllCourse()
+        self.rp.clickAllCourse()
         
         self.driver.quit()
